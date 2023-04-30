1981e78425fec7477c5c3f4ba09ad0ed42b855fe
David onuche <110064207+Davidbeebest@users.noreply.github.com>
2023-04-30 16:07:07 +0100

    animatedcontainer

--- lib/screens/screen_one.dart
+++ lib/screens/screen_one.dart
@@ -1,36 +1,46 @@
 import 'package:flutter/material.dart';
 
-import 'screen_two.dart';
-
-class ScreenOne extends StatelessWidget {
+class ScreenOne extends StatefulWidget {
   const ScreenOne({super.key});
 
+  @override
+  State<ScreenOne> createState() => _ScreenOneState();
+}
+
+class _ScreenOneState extends State<ScreenOne> {
+  bool _isAnimated = false;
+  double _width = 100;
+  double _height = 100;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch, 
-            children: [
-              Hero(
-                tag: "My Hero",
-                child: Container(
-                        width: 200,
-                        height: 200,
-                        color: Colors.amber,
-                ),
-              ),
-              SizedBox(height: 10),
-              TextButton(
-          onPressed: () => Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => ScreenTwo(),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            AnimatedContainer(
+              duration: Duration(milliseconds: 500),
+              width: _width,
+              height: _height,
+              color: _isAnimated ? Colors.amber : Colors.deepPurple,
             ),
-          ),
-          child: Text("Go to screen 2"),
-              ),
-            ]),
-        ));
+            SizedBox(height: 10),
+            TextButton(
+              onPressed: () {
+                if (!_isAnimated) {
+                  _height = 300;
+                  _width = 300;
+                } else {
+                  _height = 100;
+                  _width = 100;
+                }
+                _isAnimated = _isAnimated;
+                setState(() {});
+              },
+              child: Text("Animate"),
+            ),
+          ]),
+    ));
   }
 }
